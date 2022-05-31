900f8340509814b196de946f6b6ca1499eda7803
BartKnucle <sjarde@yahoo.fr>
2022-05-31 16:49:56 +0200

    First Commit

---
+++ Runtime/JSONNodeEvent.cs
@@ -0,0 +1,10 @@
+using FunkySheep.SimpleJSON;
+using UnityEngine;
+
+namespace FunkySheep.Events
+{
+    [CreateAssetMenu(menuName = "FunkySheep/Events/Json Node")]
+    public class JSONNodeEvent : FunkySheep.Events.Event<JSONNode>
+    {
+    }
+}
---
+++ Runtime/JSONNodeListener.cs
@@ -0,0 +1,10 @@
+using FunkySheep.SimpleJSON;
+using UnityEngine;
+
+namespace FunkySheep.Events
+{
+    [AddComponentMenu("FunkySheep/Listeners/JSON Node")]
+    public class JSONNodeListener : FunkySheep.Events.Listener<JSONNode>
+    {
+    }
+}
